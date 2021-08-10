051f7da3612419212e58dc04b5e6e66a905c7a87
KNicole <38897127+Katynicole89@users.noreply.github.com>
2021-08-10 00:09:34 -0500

    Done other then pulling from localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 //Set todays date for the top of page using Moment
-let todayDate = moment().format('dddd, MMM Do YYYY');
+let todayDate = moment().format('dddd, MMM Do YYYY, h:mm:ss a');
 $("#currentDay").html(todayDate);
 
 
@@ -11,6 +11,8 @@ $(document).ready(function()  {
     classChange();
 });
 
+
+//Function to color code blocks by changing classes
 function classChange () {
     
     var currentTime = moment().hours();
@@ -18,10 +20,10 @@ function classChange () {
     //TESTING IF IT WORKS
     console.log("Current Time"  + currentTime);
        
-// Comparing set time to now so we can change classes based on this
+// Comparing set time using IDs to time now so we can change classes based on this
     $(".description").each(function () {
         var setTime = parseInt($(this).attr("id"));
-        console.log(scheduledTime);
+        console.log(setTime);
 
         if (currentTime > setTime) {
             $(this).removeClass("future");
@@ -38,3 +40,27 @@ function classChange () {
         }
     });
 }
+
+
+//When I click on the save button - It takes the info from input and hour then saves to LocalStorage
+$(".saveBtn").click(function() {
+
+//Whats wrote inside the timeblocks
+    blockInput = $(this).siblings(".description").val().trim();
+    console.log(blockInput);
+//The .time hardcoded inside
+    blockTime = $(this).parent().attr("id");
+    console.log(blockTime);
+
+    //Save to localstorage
+    localStorage.setItem(blockInput, JSON.stringify(blockInput));
+    localStorage.setItem(blockTime, JSON.stringify(blockTime));
+
+
+    //Still working on getting localstorage info - Not working.
+    localStorage.getItem("blockInput");
+    localStorage.getItem("blockTime");
+
+
+});
+
